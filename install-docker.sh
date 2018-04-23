9df79d5edc540163cf2db871b1f4c67596fffa63
Victor Yap <victor.yap@alumni.concordia.ca>
2018-04-23 10:07:52 -0400

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+if apt-key fingerprint 0EBFCD88 | grep "9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88" | wc -l
+then
+    add-apt-repository \
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable"
+    apt-get update
+    apt-get install docker-ce
+    curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+else
+    echo "Docker Signing Key Invalid."
+    echo "Failure. Quitting.
+fi
