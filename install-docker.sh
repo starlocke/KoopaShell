c7c881ac6069917da4f1c909b10203a824205710
Victor Yap <victor.yap@alumni.concordia.ca>
2018-04-23 10:13:06 -0400

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install \
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -14,7 +14,7 @@ then
         $(lsb_release -cs) \
         stable"
     apt-get update
-    apt-get install docker-ce
+    apt-get install -y docker-ce
     curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 else
